4ba6b22c67bac8e12ca1a90c5e7ca7ae375e5181
Felipe Santos <felipecassiors@gmail.com>
2024-06-27 20:50:18 -0300

    macos?

--- scripts/build-binaries.sh
+++ scripts/build-binaries.sh
@@ -1,12 +1,15 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-set -euxo pipefail
+set -eux
 
-function build_binary() {
-    local version=$1
-    local docker_platform=$2
+version="$1"
 
-    docker build . -f - -o . --platform="${docker_platform}" --build-arg VERSION="${version}" <<'EOF'
+os="$(uname -s)"
+if [ "${os}" = "Linux" ]; then
+    build_binary() {
+        docker_platform="$1"
+
+        docker build . -f - -o . --platform="${docker_platform}" --build-arg VERSION="${version}" <<'EOF'
 FROM pkgxdev/pkgx:latest AS build
 
 WORKDIR /wd
@@ -21,14 +24,50 @@ FROM scratch
 ARG FILENAME
 COPY --from=build /wd/pkgx /
 EOF
-}
+    }
 
-version=$1
+    build_binary linux/amd64
+    pkgx +xz tar cJf "pkgx-$version+linux+x86-64.tar.xz" pkgx
+    rm -f pkgx
 
-build_binary "$version" "linux/amd64"
-pkgx +xz tar cJf "pkgx-$version+linux+x86-64.tar.xz" pkgx
-rm -f pkgx
+    build_binary linux/arm64
+    pkgx +xz tar cJf "pkgx-$version+linux+aarch64.tar.xz" pkgx
+    rm -f pkgx
+elif [ "${os}" = "Darwin" ]; then
+    build_binary() {
+        macos_arch="$1"
 
-build_binary "$version" "linux/arm64"
-pkgx +xz tar cJf "pkgx-$version+linux+aarch64.tar.xz" pkgx
-rm -f pkgx
+        rm -rf "${PKGX_DIR:-"${HOME}/.pkgx"}" "${HOME}/.cache/pkgx" "${HOME}/.local/share/pkgx"
+        env which pkgx | sort -u | xargs -r -t rm -f
+
+        arch "-${macos_arch}" zsh -s -- <<'EOF' "$version"
+set -exo pipefail
+
+version="$1"
+
+curl -fsS https://pkgx.sh | sh
+
+source <(pkgx --shellcode)
+
+dev
+
+deno task compile
+
+dev off
+EOF
+
+        rm -rf "${PKGX_DIR:-"${HOME}/.pkgx"}" "${HOME}/.cache/pkgx" "${HOME}/.local/share/pkgx"
+        env which pkgx | sort -u | xargs -r -t rm -f
+        curl -fsS https://pkgx.sh | sh
+    }
+
+    build_binary x86_64
+    pkgx +xz tar cJf "pkgx-$version+darwin+x86-64.tar.xz" pkgx
+    rm -f pkgx
+    build_binary arm64
+    pkgx +xz tar cJf "pkgx-$version+darwin+aarch64.tar.xz" pkgx
+    rm -f pkgx
+else
+    echo "Unsupported OS: ${os}"
+    exit 1
+fi
